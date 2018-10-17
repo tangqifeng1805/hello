3d9c32abc29a328d4a15aeb9371cb2ed605e7e6c
tangqifeng1805 <1024437501@qq.com>
2018-10-17 10:08:08 +0800

    text

---
+++ jt-cart/src/main/java/com/example/demo/JtCartApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtCartApplication.class, args);
+	}
+}
